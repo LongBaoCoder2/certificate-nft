f7846ee51fe1bd475529a8d4850508c6017d1013
LongBaoCoder2 <longbao.it@gmail.com>
2024-08-26 16:55:11 +0700

    feat(contract): ERC721Enumerable implement for contract

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -4,32 +4,42 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 
-contract Certificate is ERC721, ERC721URIStorage, ERC721Burnable, Ownable  {
+contract Certificate is ERC721, ERC721URIStorage, ERC721Burnable, Ownable, ERC721Enumerable  {
     uint256 private _nextTokenId;
 
     constructor()
-        ERC721("Certificate", "MTK")
+        ERC721("MyToken", "MTK")
         Ownable(msg.sender)
-    {
-        console.log("Initialize Certificate. initialOwner: ", msg.sender);
-    }
+    {}
 
-    function safeMint(address to, string memory uri) public onlyOwner  {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-
-        console.log("safeMint Call:");
-        console.log("\t to - ", to);
-        console.log("\t uri - ", uri);
     }
 
     // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
 
     function tokenURI(uint256 tokenId)
         public
@@ -43,7 +53,7 @@ contract Certificate is ERC721, ERC721URIStorage, ERC721Burnable, Ownable  {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
